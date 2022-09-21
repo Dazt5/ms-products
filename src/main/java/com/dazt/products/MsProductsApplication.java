93bd7815ac3927a83a642280c33e3846f212e24d
David Daniel Alvarez Torrecilla <dalvarezt.globant@bancochile.cl>
2022-09-21 18:08:13 -0500

    feat: jpa configuration

--- src/main/java/com/dazt/products/MsProductsApplication.java
+++ src/main/java/com/dazt/products/MsProductsApplication.java
@@ -2,8 +2,14 @@ package com.dazt.products;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.dazt"})
+@EntityScan(basePackages = {"com.dazt"})
+@EnableJpaRepositories(basePackages = {"com.dazt"})
 public class MsProductsApplication {
 
 	public static void main(String[] args) {
